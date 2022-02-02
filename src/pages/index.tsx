2eaeecd6a6760c0757944eb4aa105c49310af6db
Felipe Defacio <phelipe2208@gmail.com>
2022-02-01 21:35:54 -0300

    feat: header and signInButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
-import styles from '../styles/home.module.scss';
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function Home() {
   return (
-   <h1 className={styles.title}> 
-   <Head>
-      <title>Início | ig.news</title>
-    </Head>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
 
-   Hello World
-   </h1>
-  )
+      <h1>Hello World</h1>
+    </>
+  );
 }
