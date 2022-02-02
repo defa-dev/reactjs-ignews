2485b0bd45a02c084ff05e19d85c6118e69b7520
Felipe Defacio <phelipe2208@gmail.com>
2022-02-02 16:15:36 -0300

    feat: add stripe and nextauth

--- src/components/SigInButton/index.tsx
+++ src/components/SigInButton/index.tsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa";
 import { FiX } from 'react-icons/fi'
 import styles from "./styles.module.scss";
 export function SigInButton() {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
   
   return isUserLoggedIn ? (
     <button type="button" className={styles.sigInButton}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,60 @@
 import Head from "next/head";
+import { GetStaticProps } from "next";
+import { stripe } from "../services/stripe";
+import { SubscribeButton } from "../components/SubscribeButton";
 
-export default function Home() {
+import styles from "./home.module.scss";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>✌️ Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
 
-      <h1>Hello World</h1>
+          <SubscribeButton priceId={product.priceId}/>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KOnFKHIJTzkhGqWaBWuRuPN", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
